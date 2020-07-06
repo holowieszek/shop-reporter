895812fbf9823f4bc94ab90d1763293ec1b32c5a
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-06 23:11:20 +0200

    Added rest of CRUD endpoints

--- perfume/src/common/dbAdapterDynamoDb.js
+++ perfume/src/common/dbAdapterDynamoDb.js
@@ -1,18 +1,41 @@
-const getAll = async () => {
-  const result = [
-    { id: 1, title: 'Lorem Ipsum' },
-    { id: 2, title: 'Lorem Ipsum' },
-    { id: 3, title: 'Lorem Ipsum' },
-    { id: 4, title: 'Lorem Ipsum' },
-  ]
-  return result
-}
+const records = [
+  { id: 1, title: 'Lorem Ipsum' },
+  { id: 2, title: 'Lorem Ipsum' },
+  { id: 3, title: 'Lorem Ipsum' },
+  { id: 4, title: 'Lorem Ipsum' },
+]
+
+const getAll = async () => records
 
 const create = async (data) => {
   console.log(data);
   return data;
 }
+
+const getById = async (id) => records.filter((record) => record.id === id)
+
+const update = async (id, data) => {
+  console.log(id, data)
+  const result = {
+    message: 'Updated',
+    data
+  }
+  return result
+}
+
+const remove = async (id) => {
+  console.log(id)
+  const result = {
+    message: 'Removed',
+    ...id
+  }
+  return result
+}
+
 module.exports = {
   getAll,
-  create
+  create,
+  getById,
+  update,
+  remove
 }
--- perfume/src/common/persistence.js
+++ perfume/src/common/persistence.js
@@ -2,7 +2,16 @@ const getAll = async (dbAdapter) => dbAdapter.getAll()
 
 const create = async (dbAdapter, data) => dbAdapter.create(data)
 
+const getById = async (dbAdapter, id) => dbAdapter.getById(id)
+
+const update = async (dbAdapter, id, data) => dbAdapter.update(id, data)
+
+const remove = async (dbAdapter, id) => dbAdapter.remove(id)
+
 module.exports = {
   getAll,
-  create
+  create,
+  getById,
+  update,
+  remove
 }
