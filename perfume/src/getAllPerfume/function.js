bbe6bb48230f3169cd40390540c455f1f1bcf5f5
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-06 21:39:32 +0200

    Added template of getAllPerfume lambda

---
+++ perfume/src/getAllPerfume/function.js
@@ -0,0 +1,11 @@
+const { returnResponse } = require('../common/returnResponse');
+
+const invoke = async () => {
+  const result = {
+    function: 'getAllPerfume'
+  };
+
+  return returnResponse(result);
+}
+
+exports.invoke = invoke;
