fe6ddb64f4a4baca8b5ec31054af540df0c466bd
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-06 22:30:12 +0200

    Added function for the createPerfume lambda

---
+++ perfume/src/createPerfume/function.js
@@ -0,0 +1,12 @@
+const persistence = require('../common/persistence')
+const dbAdapter = require('../common/dbAdapterDynamoDb')
+const { returnResponse } = require('../common/returnResponse');
+
+const invoke = async (event) => {
+  const data = JSON.parse(event.body)
+  const results = await persistence.create(dbAdapter, data)
+
+  return returnResponse(results, 201);
+};
+
+exports.invoke = invoke;
