82f4bbaaefa700816973d302d89c6f42649657a9
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-17 23:08:42 +0200

    Added error handling to the getAllPerfume lambda

--- perfume/src/getAllPerfume/function.js
+++ perfume/src/getAllPerfume/function.js
@@ -1,11 +1,12 @@
 const persistence = require('../common/persistence')
 const dbAdapter = require('../common/dbAdapterDynamoDb')
 const returnResponse = require('../common/returnResponse');
+const asyncWrapper = require('../common/asyncWrapper')
 
 const invoke = async () => {
-  const results = await persistence.getAll(dbAdapter)
+  const { error, result } = await asyncWrapper(persistence.getAll(dbAdapter))
 
-  return returnResponse(results);
+  return !error ? returnResponse(result) : returnResponse({ error: 'Something went wrong!' }, 400);
 };
 
 exports.invoke = invoke;
