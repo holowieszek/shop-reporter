f50c8bdad1cc917961be44e68d82ebec40f17b44
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-17 22:25:10 +0200

    Added log helper

---
+++ perfume/src/common/log.js
@@ -0,0 +1,49 @@
+const LogLevels = {
+  DEBUG: 0,
+  INFO: 1,
+  WARN: 2,
+  ERROR: 3
+};
+
+const logLevelName = process.env.logLevel || 'DEBUG'
+
+const debug = (msg, params) => log('DEBUG', msg, params);
+const info = (msg, params) => log('INFO', msg, params);
+const warn = (msg, params, error) => log('WARN', msg, appendError(params, error));
+const error = (msg, params, error) => log('ERROR', msg, appendError(params, error));
+
+const log = (levelName, message, params) => {
+  console.log('INITIALIZED LOG');
+  if (shouldNotLog(levelName)) {
+    return;
+  }
+
+  const logMsg = {
+    message,
+    level: levelName,
+    ...params
+  }
+
+  // eslint-disable-next-line prefer-template
+  console.log(message, JSON.stringify(logMsg) + ' \n');
+}
+
+const shouldNotLog = (level) => LogLevels[level] < LogLevels[logLevelName]
+
+const appendError = (params, err) => {
+  if (!err) {
+    return params;
+  }
+
+  return Object.assign(
+    params || {},
+    { errorName: err.name, errorMessage: err.message, stackTrace: err.stack }
+  );
+}
+
+module.exports = {
+  debug,
+  info,
+  warn,
+  error
+}
