6f8a943f876bc2c0dd3200fa97cf176c878d8ba8
Mateusz Holowieszko <holowieszko.m@gmail.com>
2020-07-17 22:24:43 +0200

    Added error handling to the createPerfume lambda

--- perfume/src/createPerfume/function.js
+++ perfume/src/createPerfume/function.js
@@ -1,12 +1,21 @@
 const persistence = require('../common/persistence')
 const dbAdapter = require('../common/dbAdapterDynamoDb')
+const asyncWrapper = require('../common/asyncWrapper')
 const returnResponse = require('../common/returnResponse');
+const validator = require('./validator')
 
 const invoke = async (event) => {
   const data = JSON.parse(event.body)
-  const results = await persistence.create(dbAdapter, data)
+  const validate = validator(data)
 
-  return returnResponse(results, 201);
+  if (validate.error) {
+    const msg = validate.error.details[0].message
+    return returnResponse({ error: msg }, 400)
+  }
+
+  const { error, result } = await asyncWrapper(persistence.create(dbAdapter, data))
+
+  return !error ? returnResponse(result) : returnResponse({ error: 'Something went wrong!' }, 400);
 };
 
 exports.invoke = invoke;
